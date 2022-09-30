bd6cd11fc21e3c01428dd63263657557df820c40
Diwas Atreya <74746579+diwasatreya@users.noreply.github.com>
2022-09-30 11:42:24 +0545

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
-## Facebook:
+## Configration
 
-## Youtube:
+- Enter your bot token in .env file with `TOKEN` variable
+```js
+{
+  "prefix": '!',
+  "owner":["YOUR DISCORD ID"],
+  "nodes": [{
+    "name":"v1",
+    "host":"localhost", // your lavalink host
+    "port": 6969, // Your lavalink port
+    "password": "iamnotdumb" // Your lavalink pass
+  }]
+}
+```
+### Remove Command
+- To remove `loop` command delete `/commands/Music/loop.js` file
 
-## Discord
+- To remove `join` command delete `/commands/Music/Join.js` file
+  
+*remove the `.js` file to remove command*
 
-## Fiverr:
+### Change Status
+- Goto `/events/ready.js` in line number `8`
 
-## Twitter:
+### Change Messages
+- You can goto `/events/messageCreate.js` && `interactionCreate.js` to change some messages
 
-## Instagram
+## Thanks for Using <3 
 
+
+### Need help?
+If you need help ! Feel free to join our [Support server](https://aromaxdev.xyz/discord)
+
+
+
+
+### Music Package
+Made using [Poru](https://npmjs.com/poru) npm package
---
+++ index.js
@@ -0,0 +1,52 @@
+const discord = require("discord.js");
+const { Poru } = require("poru");
+require("dotenv").config()
+const client = new discord.Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [
+    "GUILDS","GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+});
+client.login(process.env.TOKEN).catch(e => console.log("No token provided"))
+// Atreya#2401 [diwasatreya]
+
+client.config = require("./config.json");
+client.poru = new Poru(client, client.config.nodes,{
+  spotify:{
+  clientID:"cb41529dc3bd4d8f8a240dbee0fff4e8",
+  clientSecret:"bcca82f42930498aa385a8289fdf276b",
+  playlistLimit:5
+   },
+  apple:{
+    playlistLimit:5
+  }
+  
+})
+
+client.commands = new discord.Collection();
+client.aliases = new discord.Collection();
+client.slash = new discord.Collection();
+
+//now creating interaction event
+['commands', 'events', 'slash', 'PoruEvent'].forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
+});
+
+
+
+
+require('node:http')
+  .createServer((_, res) =>
+    res.end(
+      `Developed by <b><a href="fiverr.com/diwasatreya">diwasatreya</a></b> `,
+    ),
+  )
+  .listen(8080);
